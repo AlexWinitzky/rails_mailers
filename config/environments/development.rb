9d48309375e9f7b404a036fb977eb512691a0c7b
Alex Winitzky <alex.winitzky@gmail.com>
2020-01-16 16:36:10 -0700

    update production.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,35 +1,7 @@
 Rails.application.configure do
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3001, protocol: 'http' }
+  config.action_mailer.default_url_options = { host: "localhost: 3001" }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:                "smtp.gmail.com",
-    port:                   587,
-    domain:                 "gmail.com",
-    user_name:              ENV['GMAIL_USERNAME'],
-    password:               ENV['GMAIL_PASSWORD'],
-    authentication:         :plain,
-    enable_starttls_auto:   true,
-  }
-  
-  # using SendGrid's Ruby Library
-  # https://github.com/sendgrid/sendgrid-ruby
-  
-  # require 'sendgrid-ruby'
-  # include SendGrid
-# from = Email.new(email: 'test@example.com')
-# to = Email.new(email: 'alex.winitzky@gmail.com')
-# subject = 'Sending with SendGrid is Fun'
-# content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-# mail = Mail.new(from, subject, to, content)
-
-# sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-# response = sg.client.mail._('send').post(request_body: mail.to_json)
-# puts response.status_code
-# puts response.body
-# puts response.headers
-  
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -41,7 +13,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  # config.consider_all_requests_local = true
+  config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
@@ -60,6 +32,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
@@ -74,6 +47,7 @@ Rails.application.configure do
 
   # rails mailers using letter opener
   config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 
 
   # Raises error for missing translations.
